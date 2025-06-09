74ce32e744c62bddcf3ddc2a0fcc947c61d14978
JosueUN <a24310225@live.ceti.mx>
2025-06-09 10:57:40 -0600

    dvd bounces

--- src/06_Primitivas.cpp
+++ src/06_Primitivas.cpp
@@ -1,8 +1,27 @@
 #include <SFML/Graphics.hpp>
-
+//Fernando Josue Ureña Navarro 2°K 09/06/2025
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+    sf::RenderWindow window(sf::VideoMode(1800, 1200), "SFML Window");
+    window.setFramerateLimit(60);
+
+    // Posición inicial
+    float posX = 100;
+    float posY = 100;
+    // Velocidad en cada eje
+    float velX = 2;
+    float velY = 2;
+
+    // Cargar la textura de la imagen dvd
+    sf::Texture dvdTexture;
+    if (!dvdTexture.loadFromFile("assets/images/dvd.png")) {
+        return -1;
+    }
+    sf::Sprite dvdSprite(dvdTexture);
+    dvdSprite.setPosition(posX, posY);
+
+    // Obtener tamaño del sprite
+    sf::FloatRect spriteBounds = dvdSprite.getLocalBounds();
 
     while (window.isOpen())
     {
@@ -13,27 +32,32 @@ int main()
                 window.close();
         }
 
+        // Actualizar posición
+        posX += velX;
+        posY += velY;
+
+        // Rebote en los bordes
+        if (posX < 0) {
+            posX = 0;
+            velX = -velX;
+        }
+        if (posX + spriteBounds.width > window.getSize().x) {
+            posX = window.getSize().x - spriteBounds.width;
+            velX = -velX;
+        }
+        if (posY < 0) {
+            posY = 0;
+            velY = -velY;
+        }
+        if (posY + spriteBounds.height > window.getSize().y) {
+            posY = window.getSize().y - spriteBounds.height;
+            velY = -velY;
+        }
+
+        dvdSprite.setPosition(posX, posY);
+
         window.clear();
-
-        // Dibujar primitivas básicas
-        sf::CircleShape circle(50);
-        circle.setFillColor(sf::Color::Red);
-        circle.setPosition(100, 100);
-        window.draw(circle);
-
-        sf::RectangleShape rectangle(sf::Vector2f(200, 100));
-        rectangle.setFillColor(sf::Color::Green);
-        rectangle.setPosition(300, 200);
-        window.draw(rectangle);
-
-        sf::ConvexShape triangle;
-        triangle.setPointCount(3);
-        triangle.setPoint(0, sf::Vector2f(100, 300));
-        triangle.setPoint(1, sf::Vector2f(200, 300));
-        triangle.setPoint(2, sf::Vector2f(150, 400));
-        triangle.setFillColor(sf::Color::Blue);
-        window.draw(triangle);
-
+        window.draw(dvdSprite);
         window.display();
     }
 
